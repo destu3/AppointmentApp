cc06e68ddd95f9104a124e06e307072580e35407
Destiny Uduebholo <obose03@gmail.com>
2022-02-19 14:38:10 +0000

    Added a lot of the methods and needed functionality

--- src/main/java/com/example/model/Appointment.java
+++ src/main/java/com/example/model/Appointment.java
@@ -1,16 +1,90 @@
 package com.example.model;
 
 /**
- * Write a description of class Appointment here.
+ * Appointments class
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 public class Appointment
 {
-    // instance variables 
+    // The treatment status for the appointment
     public String Status;
+
+    // The type of service required from the user
     public String Service;
+
+    // The time od the appointment
     public String Time;
+
+    // The date for the appointment
     public String Date;
+
+    // The optician for the appointment
+    public Optician optician;
+
+
+    /**
+     * Constructor for Appointment Objects
+     *
+     */
+    public Appointment()
+    {
+        this.Status = "Pending";
+        Service = "Eye Check";
+        Time = "default";
+        Date = "default";
+    }
+
+    /**
+     * Constructor for objects of class Appointment
+     *
+     * @param service  treatment status
+     * @param time     Service required
+     * @param time     decided time for appointment
+     * @param date     decided date for appointment
+     */
+    public Appointment(String Status ,String service, String time, String date)
+    {
+        this.Status = "Pending";
+        this.Service = service;
+        this.Time = time;
+        this.Date = date;
+    }
+
+    /**
+     * returns the treatment status
+     *
+     * @return  the status
+     */
+    public String getStatus() {return Status;}
+
+    /**
+     * returns the Service
+     *
+     * @return  service
+     */
+    public String getService () {return Service;}
+
+    /**
+     * returns the appointment date
+     *
+     * @return  appointment date
+     */
+    public String getDate () {return Date;}
+
+    /**
+     * returns the appointment time
+     *
+     * @return  appointment time
+     */
+    public String getTime () {return Time;}
+
+
+
+
+
+
 }
+
+
--- src/main/java/com/example/model/Patient.java
+++ src/main/java/com/example/model/Patient.java
@@ -1,34 +1,86 @@
 package com.example.model;
 
 /**
- * Write a description of class Patient here.
+ * Patient Class
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 public class Patient
 {
+    // Firstname of patient
    public String FirstName;
-   public Appointment Appointmnets;
-   public String Surname; 
+
+   // Array to hold appointments
+   private Appointment[] appointments;
+
+   // Maximum amount of appointments
+    private final int max_appointments = 5;
+
+   // The number of appointments
+    public int NumberOfAppointments;
+
+   //Surname of patient
+   public String Surname;
+
+   // Patients Registration Status
    public boolean Registered;
+
+   // The patients account username
    public String Username;
+
+   // The patients account password
    public String Password;
-   
+
+   // The patients unique identifier
+   public int UserID;
+
+
+
+   /** Constructor STILL TO BE CODED PROPERLY, the patients firstname and surname will be the values they choose during the registration process*/
+
+   /**
+     * Constructor for Patient Object
+     *
+     * @param FirstName The patients First Name
+     * @param Surname The patients surname
+     * @param Registered An indication whether the patient is registered or not
+     */
+
    public Patient(String FirstName, String Surname, boolean Registered)
-    {
-        // initialise instance variables
-        this.FirstName = FirstName;
-        this.Surname = Surname;
-        this.Registered = Registered;
-    }
-    
+   {
+       appointments = new Appointment[max_appointments];
+       NumberOfAppointments = 0;
+       this.FirstName = FirstName;
+       this.Surname = Surname;
+       this.Registered = Registered;
+   }
+
+   public Patient()
+   {
+       appointments = new Appointment[max_appointments];
+       NumberOfAppointments = 0;
+       this.FirstName = "FirstName";
+       this.Surname = "Surname";
+       this.Registered = false;
+   }
+
+   /**
+     * Displays the patients details
+     *
+     * @return Firstname and Surname
+     */
     public String displayDetails ()
     {
         System.out.format("Patient Details - %s\n", FirstName + " " + Surname);
-        return "Patient details displayed";
+        return this.FirstName + "" + Surname;
     }
 
+    /**
+     * Displays patients registration Status
+     *
+     * @return
+     */
     public void getRegistrationStatus ()
     {
         if (Registered == true)
@@ -38,22 +90,71 @@ public class Patient
         else System.out.format("Patient is not registered. Please Sign up now\n");
     }
 
-    /** This main method is only to test the method and test commiting and pushing */
-
-    public static void main(String[] args)
+    /**
+     * Displays the number of appointments
+     *
+     * @return NumberOfAppointments
+     */
+    public int getNumberOfAppointments()
     {
-        Patient patient1 = new Patient("Destiny", "Uduebholo",false);
-        patient1.displayDetails();
-
-        patient1.getRegistrationStatus();
+        System.out.format("Current number of appointments is: %s\n" , NumberOfAppointments);
+        return NumberOfAppointments;
     }
-    
-    
-    
-    
-    
 
-  
+    /**
+     * Prints information about the account and any current appointments
+     *
+     */
+    public String viewAppointments()
+    {
+        System.out.format("Firstname: %s\nSurname: %s\n", FirstName, Surname);
+
+        for(int i=0;i<NumberOfAppointments;i++)
+        {
+            Appointment appointment = appointments[i];
+
+            System.out.format("%s   %s    %s    %s\n",
+                    appointment.getStatus(),
+                    appointment.getService(),
+                    appointment.getTime(),
+                    appointment.getDate());
+        }
+        return  "";
+    }
+
+    /**
+     * Adds a new appointment as long as the maximum of 5 has not been reached
+     *
+     */
+    public String bookAppointment(Appointment newAppointment)
+    {
+        if(NumberOfAppointments < max_appointments)
+        {
+            appointments[NumberOfAppointments] = newAppointment;
+            NumberOfAppointments++;
+        }
+
+        System.out.format("Appointment successfully booked %s\n" , newAppointment);
+        return "";
+
+    }
+
+
+
+    /** Registration code will go in here */
+    public void Register ()
+    {
+
+
+    }
+
+    /** Login code will go in here */
+    public void Login ()
+    {
+
+
+    }
+
 
 }
    
